7d324aab42e342d6e165c6e902277a7f1cdd1222
Sai Raghava Katepally <ksairaghava1@gmail.com>
2020-04-13 01:24:41 +0530

    RestTemplate bean creation

--- src/main/java/com/sairaghava/MainApplication.java
+++ src/main/java/com/sairaghava/MainApplication.java
@@ -2,12 +2,19 @@ package com.sairaghava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MainApplication {
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
-
 }
